0eee6b3cab271384c4d0bed39442c5773d9f72bc
Ander Conal <ander.conal@gmail.com>
2018-01-04 23:48:45 +0100

    Conventional Routes

---
+++ asp-net-core-empty/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcoreempty.Controllers
+{
+    public class AboutController
+    {
+        public String Name()
+        {
+            return "Ander Conal";
+        }
+
+        public String Company()
+        {
+            return "Plain Concepts";
+        }
+    }
+}

035b56ff39b3e1205f4ae00fcb6a5b7a0440b044
Ander Conal <ander.conal@gmail.com>
2018-01-04 23:57:40 +0100

    Attribute Routes

--- asp-net-core-empty/Controllers/AboutController.cs
+++ asp-net-core-empty/Controllers/AboutController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace aspnetcoreempty.Controllers
 {
+    // /about
+
+    [Route("[controller]/[action]")]
     public class AboutController
-    {
+    {   
         public String Name()
         {
             return "Ander Conal";
